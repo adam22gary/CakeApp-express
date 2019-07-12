dba07d1677f78d0235de04dafd17187af30e16ce
antonella <antonella.makahe@gmail.com>
2019-07-12 14:56:45 +1000

    testing

--- controllers/ingredient_controller.js
+++ controllers/ingredient_controller.js
@@ -4,33 +4,33 @@ async function create(req, res) {
     //const theModel = new Ingredient();
     const { ingredients_name, ingredients_quantity, ingredients_measurement, ingredients_price } = req.body;
     await Ingredient.create({ ingredients_name, ingredients_quantity, ingredients_measurement, ingredients_price });
-    Ingredient.find({}, function(err, list){
-        if(err){
+    Ingredient.find({}, function (err, list) {
+        if (err) {
             res.send("Ingredients list is empty!");
             next();
         }
-        return res.json(list); 
+        return res.json(list);
     });
 };
 
 async function index(req, res) {
     //{} = all
-    Ingredient.find({}, function(err, list){
-        if(err){
+    Ingredient.find({}, function (err, list) {
+        if (err) {
             res.send("Ingredients list is empty!");
             next();
         }
-        return res.json(list); 
+        return res.json(list);
     });
 }
-
+console.log("hello");
 function newMethod(req, res) {
-    Ingredient.find({}, function(err, list){
-        if(err){
+    Ingredient.find({}, function (err, list) {
+        if (err) {
             res.send("Ingredients list is empty!");
             next();
         }
-        return res.json(list); 
+        return res.json(list);
     });
 }
 
